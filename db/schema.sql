d72d3f849b5bb8faf3eeb014d050f987174c380a
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-07-24 12:34:40 -0400

    error AccessDeniedError [SequelizeAccessDeniedError]: Access denied for user ''@'localhost' (using password: YES)...

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS  tech_blogs_db;
+
+CREATE DATABASE  tech_blogs_db;
---
+++ server.js
@@ -0,0 +1,21 @@
+//require express (glue)
+const express = require('express');
+//require api routes
+const routes = require('./routes');
+//require sequelize connections
+const sequelize = require('./config/connections');
+//creating a variable for the express
+const app = express();
+//setting up the port
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
