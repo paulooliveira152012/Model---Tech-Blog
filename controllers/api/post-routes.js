e892c88a9ef3623803b57f7553ef3c834844a476
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-07-29 18:57:29 -0400

    commit

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -92,15 +92,7 @@ router.post('/', withAuth, async (req, res) => {
     };
 });
 
-router.put('/upvote', withAuth, (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
-    .then(updatedVoteData => res.json(updatedVoteData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+
 
 router.put('/:id', withAuth, (req, res) => {
   Post.update(
